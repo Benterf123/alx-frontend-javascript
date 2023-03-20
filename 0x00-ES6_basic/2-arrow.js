d8f1f06480671ababb0905226523c86726c53488
Benterf123 <faithbenter@gmail.com>
2023-03-20 11:01:39 +0000

    arrow

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,9 @@
+let  getNeighborhoodsList = () => {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = function add(newNeighborhood) {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
