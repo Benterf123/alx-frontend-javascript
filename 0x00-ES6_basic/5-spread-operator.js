0c98d314079eae47c67b885ed82daf2b01532dc1
Benterf123 <faithbenter@gmail.com>
2023-03-20 14:08:21 +0000

    spread

---
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -0,0 +1,9 @@
+export default function concatArrays(array1, array2, string) {
+  const array1 = [red, blue];
+  const array2 = [brown, orange];
+  const string = "Blanket";
+
+  const combined = [...array1, ...array2, ...string];
+  return combined;
+}
+
