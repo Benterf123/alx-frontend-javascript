e1feb35812caa88c53185d96096fd7b8bf270a41
Benterf123 <105057200+Benterf123@users.noreply.github.com>
2023-03-22 20:18:39 +0300

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,23 @@
-function getResponseFromAPI() {
-    return new Promise((resolve) => {
-        let userInput = new Response();
-        resolve("Hey there, great to see you today!");
-    }).then((resolvedValue) => {
-        return resolvedValue;
-    });
-}
+// export default function getResponseFromAPI() {
+//     return new Promise((resolve) => {
+//         let userInput = new Response();
+//         resolve("Hey there, great to see you today!");
+//     }).then((resolvedValue) => {
+//         return resolvedValue;
+//     });
+// }
 
-getResponseFromAPI().then((response) => {
-    console.log(response);
-});
+// getResponseFromAPI().then((response) => {
+//     console.log(response);
+// });
+
+export default function getResponseFromAPI() {
+  const success = true;
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
+}
