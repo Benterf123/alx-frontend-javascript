f6183e082d2f2267e75d8d86a90e3f0c651291b6
Benterf123 <105057200+Benterf123@users.noreply.github.com>
2023-03-23 21:08:23 +0300

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,7 @@
-import classRoom from 0-classroom.js;
+import classRoom from './0-classroom';
 
 export default function initializeRooms() {
-return [
+ return [
     new classRoom(19),
     new classRoom(20),
     new classRoom(34),
